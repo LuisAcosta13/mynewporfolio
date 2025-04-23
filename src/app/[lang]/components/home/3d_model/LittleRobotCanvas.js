166a4de2d95445fa277a38c41c2b6cbe65dfa15b
Luis Acosta <luis@canari.studio>
2025-04-23 15:34:43 -0300

    change colors

--- src/app/[lang]/components/home/3d_model/LittleRobotCanvas.js
+++ src/app/[lang]/components/home/3d_model/LittleRobotCanvas.js
@@ -2,20 +2,36 @@
 
 import { LittleRobot } from "./littleRobot/LittleRobot"
 import { Canvas } from "@react-three/fiber"
-import { OrbitControls, Stage, Environment } from '@react-three/drei'
+import { OrbitControls, Stage } from '@react-three/drei'
 import { useTheme } from "@/app/[lang]/ThemeContext";
+import * as THREE from 'three';
 import '../../../styles/robotModel.scss'
 
-export default function LittleRobotCanvas({isThinking}) {
+export default function LittleRobotCanvas({ isThinking }) {
     const { theme } = useTheme();
     return (
         <div className="robotModel">
-            <Canvas shadows camera={{ position: [0, 1, 6], fov: 50, near: 0.1, far: 1000 }}>
+            <Canvas
+                shadows
+                camera={{ position: [0, 1, 6], fov: 50, near: 0.1, far: 1000 }}
+            >
                 <OrbitControls enableZoom={true} maxPolarAngle={Math.PI / 2} minDistance={1} maxDistance={10} />
-                <ambientLight intensity={0.1} />
+                <ambientLight intensity={theme === "light" ? 0.5 : 0.2} />
+                <directionalLight
+                    intensity={theme === "light" ? 1 : 0.6}
+                    position={[5, 10, 5]}
+                    castShadow
+                    shadow-mapSize-width={1024}
+                    shadow-mapSize-height={1024}
+                />
                 <spotLight position={[8, 0, 5]} angle={0.4} penumbra={10} intensity={theme === "dark" ? 10 : 100} />
-                <directionalLight position={[5, 5, 5]} intensity={0.1} />
-                <Stage environment={theme === "light" && "warehouse"} intensity={0.8} contactShadow={true} shadows={true}>
+                <Stage
+                    environment={theme === "light" ? "park" : "night"}
+                    intensity={0.5}
+                    preset={theme === "light" ? "soft" : "rembrandt"}
+                    contactShadow={true}
+                    shadows={true}
+                >
                     <LittleRobot isThinking={isThinking} />
                 </Stage>
                 <OrbitControls />
